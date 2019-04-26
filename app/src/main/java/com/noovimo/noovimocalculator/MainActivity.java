8b663753dd711379313fabd349f838d9bcc46d7c
Nicolas Chaillot <nicolas.chaillot@keendo.fr>
2019-04-26 15:42:49 +0200

    Improve style and fix input values

--- app/src/main/java/com/noovimo/noovimocalculator/MainActivity.java
+++ app/src/main/java/com/noovimo/noovimocalculator/MainActivity.java
@@ -2,9 +2,10 @@ package com.noovimo.noovimocalculator;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -173,7 +174,7 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener btnNetCalculateListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            if (etxtNetVendeur.getText().toString() != "") {
+            if (!TextUtils.isEmpty(etxtNetVendeur.getText())) {
                 reset();
                 net = Double.parseDouble(etxtNetVendeur.getText().toString());
                 calculateFAIFromNet(net);
@@ -191,7 +192,8 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener btnNotaireCalculateListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            if (etxtNotaireInclus.getText().toString() != "") {
+            if (!TextUtils.isEmpty(etxtNotaireInclus.getText())) {
+                Log.i("DEBUG", "je passe !!!" + etxtNotaireInclus.getText().toString());
                 reset();
                 double notaireInclus = Double.parseDouble(etxtNotaireInclus.getText().toString());
                 calculateNetFromNotaireInclus(notaireInclus);
@@ -208,7 +210,7 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener btnFAICalculateListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            if (etxtFAI.getText().toString() != "") {
+            if (!TextUtils.isEmpty(etxtFAI.getText())) {
                 reset();
                 double fai = Double.parseDouble(etxtFAI.getText().toString());
                 calculateNetFromFAI(fai);
@@ -226,7 +228,7 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener btnClientCalculateListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            if (etxtClient.getText().toString() != "") {
+            if (!TextUtils.isEmpty(etxtClient.getText())) {
                 reset();
                 double client = Double.parseDouble(etxtClient.getText().toString());
                 calculateNetFromClient(client);
